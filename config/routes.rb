9a6b5686d88097f1ab2015e98b465d8268b3a967
henrique lopes <henrique.0.lopes@hotmail.com>
2024-02-27 17:15:37 -0300

    root

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
 end
